a62d97f63d0b9d6a6946c623eb3e0812f888977d
prasannadharm <prasannadharm@gmail.com>
2018-12-23 08:39:23 +0530

    Tittle Changed

--- ActivationDemo/Activator.Designer.vb
+++ ActivationDemo/Activator.Designer.vb
@@ -19,21 +19,22 @@ Partial Class Activator
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label
-        Me.Label2 = New System.Windows.Forms.Label
-        Me.txtSerial = New System.Windows.Forms.TextBox
-        Me.txtKey = New System.Windows.Forms.TextBox
-        Me.btnActivate = New System.Windows.Forms.Button
-        Me.btnKeyGenerator = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSerial = New System.Windows.Forms.TextBox()
+        Me.txtKey = New System.Windows.Forms.TextBox()
+        Me.btnActivate = New System.Windows.Forms.Button()
+        Me.btnKeyGenerator = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(27, 9)
+        Me.Label1.Location = New System.Drawing.Point(36, 11)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.Size = New System.Drawing.Size(96, 17)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Serial number"
         '
@@ -41,35 +42,39 @@ Partial Class Activator
         '
         Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(29, 38)
+        Me.Label2.Location = New System.Drawing.Point(39, 47)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.Size = New System.Drawing.Size(95, 17)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Activation key"
         '
         'txtSerial
         '
         Me.txtSerial.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.txtSerial.Location = New System.Drawing.Point(105, 9)
+        Me.txtSerial.Location = New System.Drawing.Point(140, 11)
+        Me.txtSerial.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txtSerial.Name = "txtSerial"
         Me.txtSerial.ReadOnly = True
-        Me.txtSerial.Size = New System.Drawing.Size(194, 20)
+        Me.txtSerial.Size = New System.Drawing.Size(257, 22)
         Me.txtSerial.TabIndex = 2
         '
         'txtKey
         '
         Me.txtKey.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.txtKey.Location = New System.Drawing.Point(105, 35)
+        Me.txtKey.Location = New System.Drawing.Point(140, 43)
+        Me.txtKey.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txtKey.Name = "txtKey"
-        Me.txtKey.Size = New System.Drawing.Size(194, 20)
+        Me.txtKey.Size = New System.Drawing.Size(257, 22)
         Me.txtKey.TabIndex = 3
         '
         'btnActivate
         '
         Me.btnActivate.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnActivate.Location = New System.Drawing.Point(105, 61)
+        Me.btnActivate.Location = New System.Drawing.Point(140, 75)
+        Me.btnActivate.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnActivate.Name = "btnActivate"
-        Me.btnActivate.Size = New System.Drawing.Size(75, 23)
+        Me.btnActivate.Size = New System.Drawing.Size(100, 28)
         Me.btnActivate.TabIndex = 4
         Me.btnActivate.Text = "Activate"
         Me.btnActivate.UseVisualStyleBackColor = True
@@ -77,26 +82,28 @@ Partial Class Activator
         'btnKeyGenerator
         '
         Me.btnKeyGenerator.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnKeyGenerator.Location = New System.Drawing.Point(186, 61)
+        Me.btnKeyGenerator.Location = New System.Drawing.Point(248, 75)
+        Me.btnKeyGenerator.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnKeyGenerator.Name = "btnKeyGenerator"
-        Me.btnKeyGenerator.Size = New System.Drawing.Size(112, 23)
+        Me.btnKeyGenerator.Size = New System.Drawing.Size(149, 28)
         Me.btnKeyGenerator.TabIndex = 5
         Me.btnKeyGenerator.Text = "Run key generator"
         Me.btnKeyGenerator.UseVisualStyleBackColor = True
         '
-        'Form1
+        'Activator
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(313, 105)
+        Me.ClientSize = New System.Drawing.Size(417, 129)
         Me.Controls.Add(Me.btnKeyGenerator)
         Me.Controls.Add(Me.btnActivate)
         Me.Controls.Add(Me.txtKey)
         Me.Controls.Add(Me.txtSerial)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Activator"
+        Me.Text = "Activator"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
